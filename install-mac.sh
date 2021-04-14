5db48772a7f013ae3ca18d2a91387a95040390c4
ecascardo <ezequiel.cascardo@mercadolibre.com>
2021-04-13 23:37:22 -0300

    Update install-mac.sh

--- install-mac.sh
+++ install-mac.sh
@@ -1,10 +1,6 @@
 
-echo "Type the linux IP, followed by [ENTER]:"
-
-read ip
-
 cat > ~/tunel.sh << ENDOFFILE
 killall ssh
-ssh -o ExitOnForwardFailure=yes -f -N -R 2222:localhost:22  $ip
+ssh -o ExitOnForwardFailure=yes -f -N -R 2222:localhost:22  $1
 ENDOFFILE
 chmod +x ~/tunel.sh
